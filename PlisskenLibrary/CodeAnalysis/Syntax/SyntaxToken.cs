34b4410ad7cce84062bad1f40f4d914d06e916ee
Sean Kelly <skelly@aaski.com>
2019-07-05 15:58:15 -0400

    Fix null reference for when token in artificially inserted

--- PlisskenLibrary/CodeAnalysis/Syntax/SyntaxToken.cs
+++ PlisskenLibrary/CodeAnalysis/Syntax/SyntaxToken.cs
@@ -19,6 +19,6 @@ namespace PlisskenLibrary.CodeAnalysis.Syntax
         public int Position { get; }
         public string Text { get; }
         public object Value { get; }
-        public override TextSpan Span => new TextSpan(Position, Text.Length);
+        public override TextSpan Span => new TextSpan(Position, Text?.Length ?? 0);
     }
 }
