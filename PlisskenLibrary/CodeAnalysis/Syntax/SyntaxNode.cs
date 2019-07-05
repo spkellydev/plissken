21b6d7216ba56cbd9d7cee9bfdee6a8b60c3d52a
Sean Kelly <skelly@aaski.com>
2019-07-05 14:26:45 -0400

    Add span to all SyntaxNodes

--- PlisskenLibrary/CodeAnalysis/Syntax/SyntaxNode.cs
+++ PlisskenLibrary/CodeAnalysis/Syntax/SyntaxNode.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace PlisskenLibrary.CodeAnalysis.Syntax
@@ -6,6 +7,15 @@ namespace PlisskenLibrary.CodeAnalysis.Syntax
     public abstract class SyntaxNode
     {
         public abstract SyntaxKind Kind { get; }
+        public virtual TextSpan Span
+        {
+            get
+            {
+                var first = GetChildren().First().Span;
+                var last = GetChildren().Last().Span;
+                return TextSpan.FromBounds(first.Start, last.End);
+            }
+        }
         public IEnumerable<SyntaxNode> GetChildren()
         {
             var properties = GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
